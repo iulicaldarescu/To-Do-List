6b845d5af31e6041d42b9e0d71ef5cddfddba918
iulicaldarescu <63592656+iulicaldarescu@users.noreply.github.com>
2021-12-15 14:48:37 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,46 +1,63 @@
 let button = document.getElementById('btn1');
-let adaugare = document.getElementById("add");
 let input = document.getElementById("form");
 let ul = document.getElementById("list");
 
-
-
-
-
 var arr = [];
 
+
+
+
 button.addEventListener("click", () => {
-    
-   adaugare.innerHTML = "Adaugare element";
-   adaugare.classList.add("add2", "btn", "btn-warning");
+    run();
+
 });
 
 
-adaugare.addEventListener("click", () => {
-    input.innerHTML = "<input id = 'myInput' class= 'textInput' type= 'text' placeholder= 'Introduceti un element'></input> <button class= 'btn btn-danger' type= 'button' id='btn2'> Adauga </button>"
+function run (){
+    input.innerHTML = "<input id = 'myInput' onkeypress= 'return (event.charCode >= 65 && event.charCode <= 90) || (event.charCode >= 97 && event.charCode <= 122) || (event.charCode >= 48 && event.charCode <= 57)' class= 'textInput' type= 'text' placeholder= 'Introduceti un element'></input> <button class= 'btn btn-danger' type= 'submit' id='btn2'> Adauga </button>"
     
 
     let btn2 = document.getElementById('btn2');
+
     btn2.addEventListener('click', () => {
         let li = document.createElement('li');
         let val = document.querySelector('input').value;
-        if(val.length == 0){
-            console.log('please insert value');
-        }else{
-        arr.push(val);
-        li.appendChild(document.createTextNode(val));
-        ul.appendChild(li);
-        document.getElementById('myInput').value = '';
-        }
 
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+
+        var myList = document.getElementsByTagName("LI");
         
+        if(val.length == 0){
+                console.log('please insert value');
+            }else{
+            arr.push(val);
+            li.appendChild(document.createTextNode(val));
+            ul.appendChild(li);
+            document.getElementById('myInput').value = '';
+            
+
+            for (let i = 0; i < myList.length; i++) {
+        
+                span.className = "close";
+                span.appendChild(txt);
+                myList[i].appendChild(span);
+                }
+
+            var close = document.getElementsByClassName('close');
+
+            for(let i = 0; i < close.length; i++){
+                close[i].onclick = function(){
+                    var div = this.parentElement;
+                    div.style.display = "none";
+                }
+            }
+    }
+
+    li.addEventListener('click', function(e){
+        e.target.style.textDecoration="line-through";
+      });
+
     });
-    
-
-});
-
-
-
-
-
-
+      
+}
